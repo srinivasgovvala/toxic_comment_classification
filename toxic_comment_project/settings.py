81093941423c1837c41d38e9a89a1be055dee933
Naga srinivas Govvala <145465001+srinivasgovvala@users.noreply.github.com>
2026-07-24 11:10:17 +0530

    Update settings.py

--- toxic_comment_project/settings.py
+++ toxic_comment_project/settings.py
@@ -92,7 +92,7 @@ import dj_database_url
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        default=os.getenv("DATABASE_URL"),
         conn_max_age=600,
         conn_health_checks=True,
     )
