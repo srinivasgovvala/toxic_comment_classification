196c58d2f4dce1a4195a6a245c2e67aab6319a4f
Naga srinivas Govvala <145465001+srinivasgovvala@users.noreply.github.com>
2026-07-24 10:54:13 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,7 @@
 import os
 
-from django.core.wsgi import get_wsgi_application
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "toxic_comment_project.settings")
-
+from django.core.wsgi import get_wsgi_application
 app = get_wsgi_application()
