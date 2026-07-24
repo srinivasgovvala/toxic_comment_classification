e3074633f8f8c3d5b7b955545991f948571ab0dc
Naga srinivas Govvala <145465001+srinivasgovvala@users.noreply.github.com>
2026-07-24 11:07:07 +0530

    Update settings.py

--- toxic_comment_project/settings.py
+++ toxic_comment_project/settings.py
@@ -87,11 +87,15 @@ WSGI_APPLICATION = 'toxic_comment_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import os
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, "db.sqlite3"),
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 # Password validation
